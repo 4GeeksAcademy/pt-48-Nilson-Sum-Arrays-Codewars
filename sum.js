12574ee0127394260726be367e964081f4536b94
NilsonMarenco <137912131+NilsonMarenco@users.noreply.github.com>
2023-10-24 19:06:05 +0000

    Me da errores aún, seguiré mañana

---
+++ sum.js
@@ -0,0 +1,7 @@
+function sum (numbers) {
+    "use strict";
+    return numbers.reduce ((acumulador, valorActual) => acumulador + valorActual, 0);
+};
+
+const numbersList = [1, 5.2, 4 , 0, -1];
+const result = sum (numbersList);
